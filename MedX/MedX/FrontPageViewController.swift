9735c3755ca8a35487e712724c670b7ff19f019d
WW <wentaowu23@gmail.com>
2019-07-20 12:24:55 -0500

    merged

---
+++ MedX/MedX/FrontPageViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FrontPageViewController.swift
+//  MedX
+//
+//  Created by Xiangmin Zhang on 7/20/19.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+import UIKit
+
+class FrontPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = #colorLiteral(red: 0.5836969018, green: 0.7517433167, blue: 0.8807654977, alpha: 1)
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
