f88c2127d616bcd871782788cbef98b67198007a
Siam Abd Al-Ilah <siam.abdal-ilah@wustl.edu>
2019-07-21 01:42:29 -0500

    downloading api should be working

--- MedX/MedX/Helpers/DownloadHelper.swift
+++ MedX/MedX/Helpers/DownloadHelper.swift
@@ -10,21 +10,29 @@ import Foundation
 import Alamofire
 
 class DownloadHelper{
-    static func downloadWithMeta(meta: String, token: String, completion: @escaping (DataResponse<Any>)->()){
-        let api = LethAPI()
-        api.fetchFile(meta: meta, token: token, completion: {response in
-            write(response: response, completion: completion)
-        })
+    private static func testDownload(){
+
+//            downloadWithMeta(meta: "QmUFL1AsYXFg4hpj9wJfJJaPkLEmxTxz8NWVXabyoASAgY", token: "eyJibG9ja2NoYWluIjoiRVRIIiwiZXRoX2FkZHJlc3MiOiIweGM5MTZDZmU1YzgzZEQ0RkMzYzNCMEJmMmVjMmQ0ZTQwMTc4Mjg3NWUiLCJpYXQiOjE3NDQ5LCJlYXQiOjE4NDQ5fQ.5ldY8UNBeiGcf31LGvkZDrBq9lrU7OWxc9Ii0PdjwgNQMUNtgqwq0td10Yu47AjzHJArM0AhXbDFc9NqvxR-jwE", completion: {_ in print("done")})
     }
     
-    private static func write(response: DataResponse<Any>, completion: @escaping (DataResponse<Any>)->()){
-        let url = FileManager.default.urls(for: .documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask).first!
-        let filePath = url.appendingPathComponent("randomfilename101hahahalol.txt")
+    static func download(files: [String: String], token: String, completion: @escaping (DataResponse<Any>)->()){
+        let api = LethAPI()
+        for (name, meta) in files {
+            api.fetchFile(meta: meta, token: token, completion: {response in
+                write(fileName: name, response: response, completion: completion)
+            })
+        }
+        
+    }
+
+    private static func write(fileName: String, response: DataResponse<Any>, completion: @escaping (DataResponse<Any>)->()){
+        let filePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent(fileName)
         do {
+            print(filePath.absoluteString)
             try response.data?.write(to: filePath)
-            print("File written to disk")
+            print("\(fileName) written to disk")
         } catch {
-            print("Error writing file. completion function not executed")
+            print("Error writing file: \(fileName). completion function not executed")
             return
         }
         completion(response)
